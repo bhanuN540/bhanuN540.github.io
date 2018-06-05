953a91c44dd6e9ee7798721e64b1eb69336d1781
bhanuN540 <bhanu540@gmail.com>
2018-06-05 16:08:16 -0400

    Hotel details changes

--- ndc_hackathon/HotelDetails/main.js
+++ ndc_hackathon/HotelDetails/main.js
@@ -1,23 +1,8 @@
-/*$(document).ready(function(){
+$(document).ready(function(){
     var url_string = window.location.href
     var url = new URL(url_string);
-    code = url.searchParams.get("ac");
-    //var images= [{"url":"1.jpg"},{"url":"2.jpg"},{"url":"3.jpg"},{"url":"4.jpg"},{"url":"5.jpg"}];
-    var images=[{"url":"1.jpg"},{"url":"2.jpg"}];
-    //var images=[{"url":"1.jpg"},{"url":"2.jpg"},{"url":"3.jpg"},{"url":"4.jpg"},{"url":"5.jpg"}];
-    var path = "img/"+code+"/"+images[0].url;
+    window.hotelName = url.searchParams.get("name");
+    var path = "img/"+window.hotelName+".jpg"
     $("#img").attr("src",path);
-    count=1;
-    setInterval(function(){ 
-      dispImg(images[count].url);
-      count=count+1;
-      if(count==images.length) count=0;
-      //$("#img").fadeOut();
-    },2000);
+    $("#header").text(window.hotelName);
 });
-function dispImg(url){
-  var path = "img/"+code+"/"+url;
-  $("#img").attr("src",path);
-  //$("#img").fadeIn("500");
-  //$("#img").fadeout("slow");
-}*/
